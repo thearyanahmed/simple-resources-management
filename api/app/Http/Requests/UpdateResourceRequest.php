b3c6b9ebee5d510818cc2090415cf6986970bfa8
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-09-15 20:50:56 +0600

    added update resource request

---
+++ api/app/Http/Requests/UpdateResourceRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Validator;
+
+class UpdateResourceRequest extends CreateResourceRequest
+{
+    // check CreateResourceRequest
+}
