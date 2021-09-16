b1e93e4af00720ff47ec69dcabef65392bf0a66e
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-09-16 20:37:10 +0600

    removed create resource from Resource model

--- api/app/Models/Resource.php
+++ api/app/Models/Resource.php
@@ -37,62 +37,6 @@ class Resource extends Model
         return $this->morphTo();
     }
 
-    /**
-     * @param array $data
-     * @return Resource
-     * Use DTO? overkill?
-     * Set it in a service layer? no?
-     * @throws Throwable
-     */
-    public static function createResource(array $data): Resource // get some sort of resource factory to create ?
-    {
-        // use db transaction, multi row insertion dependent on another
-        DB::beginTransaction();
-
-        try {
-
-            $relatedResourceModel = self::getResourceableType($data['resource_type']);
-
-            $resource = [
-                'title'             => $data['title'],
-                'resourceable_type' => $relatedResourceModel,
-            ];
-
-            // create related resource
-            $relatedResourceData = Arr::except($data,['title','resource_type']);
-
-            // related resource factory to create ?
-            $relatedResource = $relatedResourceModel::create($relatedResourceData);
-
-            $resource['resourceable_id'] = $relatedResource->id;
-
-            // create resource
-            $resource = Resource::create($resource);
-
-            DB::commit();
-
-            // assign related resource with specific key
-            $resource->{$data['resource_type']} = $relatedResource;
-
-            //TODO [improvement?] use DTO ?
-            return $resource;
-
-        } catch (Throwable $e) {
-            DB::rollback();
-            throw $e;
-        }
-    }
-
-    // todo change model mapping
-    private static function getResourceableType(string $resourceType) : string
-    {
-        return ([
-            Resource::RESOURCE_LINK         => Link::class,
-            Resource::RESOURCE_FILE          => Link::class,
-            Resource::RESOURCE_HTML_SNIPPET => Link::class,
-        ])[$resourceType];
-    }
-
     public function getResourceTypeAttribute()
     {
         return ([
