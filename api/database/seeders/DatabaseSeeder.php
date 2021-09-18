a7eab7c6ffa47688cc8e3d19c06755e193a41bbf
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-09-18 20:24:30 +0600

    added seeder

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\File;
+use App\Models\HtmlSnippet;
+use App\Models\Link;
+use App\Models\Resource;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +18,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        collect([
+            Link::class, HtmlSnippet::class, File::class,
+        ])
+            ->each(function ($model){
+                $model::factory()->count(35)->create()->each(function ($link) use ($model) {
+                    Resource::factory()->count(1)->create([
+                        'resourceable_type' => $model,
+                        'resourceable_id'   => $link->id
+                    ]);
+                });
+            });
     }
 }
