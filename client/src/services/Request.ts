56b022f8b9c6d6308e3be62d8d209c6921bc6ef7
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-09-19 06:16:17 +0600

    fixed Router/buildQuery

--- client/src/services/Request.ts
+++ client/src/services/Request.ts
@@ -2,37 +2,55 @@ import axios from "axios"
 
 import Router from "./Router"
 
+type CallbackHandler = () => any
+type RequestData = object | FormData
+type Headers = object
+
 export default class Request {
+    private actingAsAdmin: boolean;
+    private onSuccess: CallbackHandler | null;
+    private onError: CallbackHandler | null;
+    private onCompletion: CallbackHandler | null;
+    private endpoint: string | null;
+    private requestData: RequestData | null;
+    private requestHeaders: Headers | null;
+
     constructor() {
         this.actingAsAdmin = false
+        this.onSuccess = null
+        this.onError = null
+        this.onCompletion = null
+        this.endpoint = null
+        this.requestData = null
+        this.requestHeaders = null
     }
 
-    success(callback) {
+    success(callback: CallbackHandler) {
         this.onSuccess = callback
         return this
     }
 
-    error(callback) {
+    error(callback: CallbackHandler) {
         this.onError = callback
         return this
     }
 
-    endsWith(callback) {
+    endsWith(callback : CallbackHandler) {
         this.onCompletion = callback
         return this
     }
 
-    to(name,params) {
+    to(name: string,params: array) {
         this.endpoint = Router.getRoute(name,params || [])
         return this
     }
 
-    with(requestData) {
+    with(requestData : RequestData) {
         this.requestData = requestData
         return this
     }
 
-    headers(data = {}) {
+    headers({ data = {}} : { data: Headers | null } ) {
         this.requestHeaders = data
         return this
     }
@@ -54,14 +72,16 @@ export default class Request {
      */
     send() {
 
-        let headers = {
+        const headers = {
             'Accept': 'application/json',
             ...this.requestHeaders
         }
 
         if(! this.actingAsAdmin) {
+            // @ts-ignore
             delete headers['user_email']
         } else {
+            // @ts-ignore
             headers['user_email'] = 'admin@admin.com'
         }
 
--- client/src/services/Router.ts
+++ client/src/services/Router.ts
@@ -2,17 +2,20 @@ import api_routes from "@/api_routes";
 
 const url = process.env.VUE_APP_API_BASE_URL
 
+interface StringMap { [key: string]: string; }
+type Query = StringMap
+
 export default class Router {
 
-    static getRoute(name, ...params ) {
-        let route = Object.assign({}, Router.findRoute(name));
+    static getRoute(name : string, ...params : any[] ) : string | null {
+        const route = Object.assign({}, Router.findRoute(name));
 
         if(!route) {
             return null
         }
         let path = route.path
 
-        path = Router.parseRouteStrings(path,[].concat.apply([], params[0]))
+        path = Router.parseRouteStrings(path,[...params[0]])
 
         route.path = path
         route.abs = url + path
@@ -31,7 +34,7 @@ export default class Router {
         return routeString
     }
 
-    static buildQuery(url, { page, perPage, query= {} } ) {
+    static buildQuery(url : string, { page = null, perPage = null, query = null } : { page : number | null, perPage : number | null, query : Query | null } ) {
 
         if(url || perPage || query) {
             url += '?'
