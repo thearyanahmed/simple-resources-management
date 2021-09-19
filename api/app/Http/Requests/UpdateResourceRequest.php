18d178e5723ebc08950b0919914ae093e1721e9b
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-09-19 19:01:51 +0600

    wip api

--- api/app/Http/Requests/UpdateResourceRequest.php
+++ api/app/Http/Requests/UpdateResourceRequest.php
@@ -8,4 +8,7 @@ use Illuminate\Support\Facades\Validator;
 class UpdateResourceRequest extends CreateResourceRequest
 {
     // check CreateResourceRequest
+    protected $fileRules = [
+        'file' => 'nullable|file|mimes:pdf|max:5120'
+    ];
 }
